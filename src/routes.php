62919a23f4787a882848d097225ccad995c58d55
Adam Berecz <adam@webaxis.hu>
2016-02-25 10:59:58 +0100

    package fixes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::post('/wax/repeater', 'Waxis\Lister\Controllers\ListerController@list');
-Route::post('/wax/repeater/more', 'Waxis\Lister\Controllers\ListerController@more');
-Route::post('/wax/repeater/delete', 'Waxis\Lister\Controllers\ListerController@delete');
-Route::post('/wax/repeater/changeorder', 'Waxis\Lister\Controllers\ListerController@changeorder');
+Route::post('/wax/repeater', 'Waxis\Repeater\RepeaterController@list');
+Route::post('/wax/repeater/more', 'Waxis\Repeater\RepeaterController@more');
+Route::post('/wax/repeater/delete', 'Waxis\Repeater\RepeaterController@delete');
+Route::post('/wax/repeater/changeorder', 'Waxis\Repeater\RepeaterController@changeorder');
