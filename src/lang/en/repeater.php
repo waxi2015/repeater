4dcb5ac32a2802c9735fef4e3023eb4ef8a27882
Adam Berecz <adam@webaxis.hu>
2016-06-22 11:56:44 +0200

    fixes

---
+++ src/lang/en/repeater.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+	
+	'success_msg_title' => 'Message',	
+	'error_msg_title' => 'Error',
+	'confirm_delete_element' => 'Are you sure you want to delete?',
+	'delete_success_msg' => 'Delete successful',
+	'order_success_msg' => 'Order changes successfully',
+	'empty_text' => 'No results',
+	'success_process' => 'Process successful',
+	'add_button' => 'Add :label',
+	'add_title' => 'Add :label',
+	'edit_title' => 'Edit :label',
+	'popup_form_save' => 'Save',
+	'popup_form_close' => 'Close',
+	'import_successful' => 'Import successful',
+	'import_failed' => 'Import failed',
+
+	'paginator' => [
+		'display_text' => 'Results: :from-:to (total: :total)',
+		'button_load_more' => 'Load more',
+		'load_more_loading' => 'Loading...',
+		'first' => '«',
+		'prev' => '&#8249;',
+		'next' => '&#8250;',
+		'last' => '»',
+	],
+];
--- src/lang/hu/repeater.php
+++ src/lang/hu/repeater.php
@@ -3,9 +3,19 @@
 return [
 	
 	'success_msg_title' => 'Üzenet',
+	'error_msg_title' => 'Error',
 	'confirm_delete_element' => 'Biztosan törli az elemet?',
 	'delete_success_msg' => 'Törlés sikeres',
 	'order_success_msg' => 'Sorrend módosítás sikeres',
+	'empty_text' => 'Nincs találat',
+	'success_process' => 'Sikeres művelet',
+	'add_button' => ':label hozzáadása',
+	'add_title' => ':label hozzáadása',
+	'edit_title' => ':label szerkesztése',
+	'popup_form_save' => 'Mentés',
+	'popup_form_close' => 'Bezár',
+	'import_successful' => 'Importálás sikeres',
+	'import_failed' => 'Hiba lépett fel az importálás során',
 
 	'paginator' => [
 		'display_text' => 'Találatok: :from-:to (összesen: :total)',
